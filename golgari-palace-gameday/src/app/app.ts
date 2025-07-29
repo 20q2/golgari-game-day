f3274275000806b3ca330d2575887c966b7337d4
20q2 <andrewnintendo@msn.com>
2025-07-29 02:02:49 -0400

    yay

--- golgari-palace-gameday/src/app/app.ts
+++ golgari-palace-gameday/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FloatingParticlesComponent } from './floating-particles/floating-particles.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FloatingParticlesComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
