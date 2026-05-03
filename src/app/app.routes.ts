e812aa6c93dbb72f44189df6192b7535a9aae4ce
Andrew <abrogan@wovensolutions.io>
2026-05-02 20:52:26 -0400

    feat(tv): register /tv route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { GamesComponent } from './games/games.component';
+import { TvComponent } from './tv/tv.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'games', component: GamesComponent },
+  { path: 'tv', component: TvComponent },
   { path: '**', redirectTo: '/home' }
 ];
